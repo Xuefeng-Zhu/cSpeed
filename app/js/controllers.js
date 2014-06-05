ff5d0461768d13d61ecabf42b46c93b915107032
Xuefeng-Zhu <xzhu15@illinois.edu>
2014-06-04 21:45:48 -0400

    add lib and basic ui

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,10 +2,12 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
-
-  }])
-  .controller('MyCtrl2', [function() {
-
-  }]);
+angular.module('myApp.controllers', [])
+.controller('TimerCtrl', function($scope, $http) {
+	$scope.startTest = function(){
+		$scope.$broadcast('timer-start');
+		$http.get("www.facebook.com").fail(function(){
+			console.log($('timer span')[0].innerText);
+		});
+	}
+});
