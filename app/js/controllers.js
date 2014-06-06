fc74efb174543a1212f859e31d04c858b48de78c
Xuefeng-Zhu <xzhu15@illinois.edu>
2014-06-06 06:27:45 -0400

    Revert "be able to run test"
    This reverts commit cb0b7036ecad565dbfb765d360816caa95510ba4.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,11 +4,58 @@
 
 angular.module('myApp.controllers', [])
 .controller('TimerCtrl', function($scope) {
+
+	$scope.tests = [{"name": "Facebook", "link": "https://www.facebook.com"},
+	{"name": "Google", "link": "https://www.google.com"},
+	{"name": "Engadget", "link": "https://www.engadget.com"},
+	{"name": "Reddit", "link": "https://www.reddit.com"},
+	{"name": "Github", "link": "https://www.github.com"},
+	{"name": "Linkedin", "link": "https://www.linkedin.com"},
+	{"name": "Youtube", "link": "https://www.youtube.com"},
+	{"name": "Yahoo", "link": "https://www.yahoo.com"},
+	{"name": "Wikipedia", "link": "https://www.wikipedia.org"},
+	{"name": "Twitter", "link": "https://www.twitter.com"}];
+
+	$('.message .close').on('click', function() {
+		$(this).closest('.message').fadeOut();
+	});
+
+	var index = 0;
+	$scope.status = "Start Test"
+
 	$scope.startTest = function(){
-		$.get("http://www.engadget.com", function(response) {
-			console.log($('timer span')[0].innerText);
-		});
+		index = 0;
+		$scope.currentTest = $scope.tests[index];
+		$scope.finishedTest = [];
 
 		$scope.$broadcast('timer-start');
+
+		loadPage($scope.currentTest["link"]);
+		$scope.status = "Test is running"
+	}
+
+	function loadPage(link){
+		$.get(link, function(response) {
+			console.log($('timer span')[0].innerText);
+			$scope.currentTest.time = $('timer span')[0].innerText;
+
+			index++;
+			if (index == $scope.tests.length){
+				$scope.currentTest = undefined;
+
+				$("#currentTest").text("Finish");
+				$scope.$broadcast('timer-stop');
+				$scope.status = "Run Again";
+				$scope.$digest();
+				return;
+			}
+
+			$scope.finishedTest.push($scope.currentTest);
+			$scope.currentTest = $scope.tests[index];
+
+			$scope.$broadcast('timer-start');
+			$scope.$digest();
+			loadPage($scope.currentTest["link"]);
+		});
 	}
 });
