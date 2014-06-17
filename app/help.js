efb8a2d0d5eb781b5fdbfb4da5d17cbf36c64c7b
Xuefeng-Zhu <xzhu15@illinois.edu>
2014-06-17 00:20:04 -0400

    working

--- app/help.js
+++ app/help.js
@@ -1,4 +1,4 @@
-  chrome.browserAction.onClicked.addListener(function(tab){
-  	var url = chrome.extension.getURL('index.html');
-  	chrome.tabs.create({url: url})
-  })
+ var url = chrome.extension.getURL('index.html');
+ chrome.tabs.create({url: url})
+ 
+
