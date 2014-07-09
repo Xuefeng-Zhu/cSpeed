ccde61cbadd747658d9a8adadc72ca3d7e3a3b6c
Xuefeng-Zhu <xzhu15@illinois.edu>
2014-07-08 22:40:34 -0400

    test message

--- example/SpaceTime-master/chrome-load-timer-master/src/timer.js
+++ example/SpaceTime-master/chrome-load-timer-master/src/timer.js
@@ -1,16 +1,20 @@
 (function() {
-    (function check() {
-        (document.readyState == "complete") ? measure() : setTimeout(check, 100);
-    })();
+    // (function check() {
+    //     (document.readyState == "complete") ? measure() : setTimeout(check, 100);
+    // })();
     
-    function measure() {
-        var t = performance.timing;
-        var start = t.redirectStart == 0 ? t.fetchStart : t.redirectStart;
-        if (t.loadEventEnd > 0) {
-            // we have only 4 chars in our disposal including decimal point
-            var time = String(((t.loadEventEnd - start) / 1000).toPrecision(3)).substring(0, 4);
-            var roe = chrome.runtime && chrome.runtime.sendMessage ? 'runtime' : 'extension';            
-            chrome[roe].sendMessage({time: time, timing: t});
-        }
-    }
+    console.log("test")
+    document.addEventListener('DOMContentLoaded', function () {
+       console.log(performance.timing) 
+}
+    // function measure() {
+    //     var t = performance.timing;
+    //     var start = t.redirectStart == 0 ? t.fetchStart : t.redirectStart;
+    //     if (t.loadEventEnd > 0) {
+    //         // we have only 4 chars in our disposal including decimal point
+    //         var time = String(((t.loadEventEnd - start) / 1000).toPrecision(3)).substring(0, 4);
+    //         var roe = chrome.runtime && chrome.runtime.sendMessage ? 'runtime' : 'extension';            
+    //         chrome[roe].sendMessage({time: time, timing: t});
+    //     }
+    // }
 })();
