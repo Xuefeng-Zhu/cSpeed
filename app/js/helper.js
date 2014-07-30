1afb619c2d391fb3bc32fa2e366d9bdb7a0db871
Xuefeng-Zhu <xzhu15@illinois.edu>
2014-07-29 20:27:32 -0400

    add_comment_to_helper.js

--- app/js/helper.js
+++ app/js/helper.js
@@ -1,3 +1,4 @@
+//This function will send timing data to extension untill the page gets loaded 
 function measure() {
     if (performance.timing.loadEventEnd == 0) {
         setTimeout(measure, 1000);
