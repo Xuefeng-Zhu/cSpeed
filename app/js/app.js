1fd3a09827e59529156a57ae20f6815f61f2ce31
Xuefeng-Zhu <xzhu15@illinois.edu>
2014-07-29 20:45:05 -0400

    remove_unnecessary_dependency

--- app/js/app.js
+++ app/js/app.js
@@ -3,16 +3,13 @@
 
 // Declare app level module which depends on filters, and services
 angular.module('myApp', [
-	'ngRoute',
-	'timer',
-	'myApp.filters',
-	'myApp.services',
-	'myApp.directives',
-	'myApp.controllers',
-	'ngAnimate', 
-	'fx.animations'
-	]);
+    'ngRoute',
+    'timer',
+    'myApp.controllers',
+    'ngAnimate',
+    'fx.animations'
+]);
 
 $('.message .close').on('click', function() {
-	$(this).closest('.message').fadeOut();
+    $(this).closest('.message').fadeOut();
 });
