c81bfc72633b6d6f903cda65959767de93771581
Xuefeng-Zhu <xzhu15@illinois.edu>
2014-08-25 19:38:44 -0500

    prompt a dialog if city cannot be identify

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -56,6 +56,10 @@ angular.module('myApp.controllers', [])
         //get user ip address and load statics to isp and region
         $http.get('http://ip-api.com/json').success(function(response) {
             //map city 
+            if (response.city == ""){
+                response.city = prompt("We cannot locate your city. Please input manually");
+            }
+
             if (sisterCity[response.city]) {
                 response.city = sisterCity[response.city];
             }
