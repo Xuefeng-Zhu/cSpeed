b4cdb3029beac9bec7fe2a37709b5f2f67c248a8
Xuefeng-Zhu <xzhu15@illinois.edu>
2014-10-01 17:01:47 -0500

    change background

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -373,7 +373,7 @@ angular.module('myApp.controllers', [])
                 chartArea: {
                     left: 200,
                 },
-                backgroundColor: "#EFEFEF",
+                // backgroundColor: "#EFEFEF",
                 tooltip: {
                     trigger: 'none'
                 }
