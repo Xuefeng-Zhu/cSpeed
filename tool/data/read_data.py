9d2328446f70ee7e948c77686de19fe091890d92
Xuefeng-Zhu <xzhu15@illinois.edu>
2014-12-03 01:15:00 -0600

    code for data reading

---
+++ tool/data/read_data.py
@@ -0,0 +1,15 @@
+import json
+
+data = None
+with open('speedtest-export.json') as f:
+	data = json.load(f)
+
+individuals = data['individuals']
+google = []
+
+for test in individuals.values():
+	google_time = test['google']['time']
+	google.append(google_time['loadEventEnd'] - google_time['navigationStart'])
+
+
+print sum(google) / len(google)
